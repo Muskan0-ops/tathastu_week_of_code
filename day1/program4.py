2841f73904c61760e4d0444c82c90c2678defc5e
Muskan0-ops <65716389+Muskan0-ops@users.noreply.github.com>
2020-05-21 18:42:37 +0530

    Create program4.py

---
+++ day1/program4.py
@@ -0,0 +1,7 @@
+def prog():
+    CP=int(input("enter cost price"))
+    SP=int(input("enter selling price"))
+    profit=SP-CP
+    print(profit)
+    sp=(CP+CP*5/100)
+    print("selling price when we want profit by 5%" ,sp)
