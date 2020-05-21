2abbfd17e8ce13656114fafab206b69662949dc1
Muskan0-ops <65716389+Muskan0-ops@users.noreply.github.com>
2020-05-21 18:03:52 +0530

    Create program1.py

---
+++ day1/program1.py
@@ -0,0 +1,12 @@
+def multiple_inputs():
+    name=input("enter name: ")
+    branch=input("enter branch: ")
+    gender=input("enter gender: ")
+    college=input("enter college: ")
+    Age=input("enter age: ")
+    print("name:" ,name)
+    print("branch:", branch)
+    print("gender:", gender)
+    print("college:", college)
+    print("age:" ,age)
+    
