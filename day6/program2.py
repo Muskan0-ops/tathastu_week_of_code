84f5040ee080ab3a5f3b2189b02bf4680e59a186
Muskan0-ops <65716389+Muskan0-ops@users.noreply.github.com>
2020-06-06 23:34:46 +0530

    Create program2.py

---
+++ day6/program2.py
@@ -0,0 +1,9 @@
+s = input("Enter string of zeroes and ones: ")
+a =""
+for i in range(len(s)):
+    if s[i] == '0':
+        a=a+'0'
+for i in range(len(a), len(s)):
+    a=a+'1'
+print("String in ascending order is: ", a)
+© 2020 GitHub, Inc.
